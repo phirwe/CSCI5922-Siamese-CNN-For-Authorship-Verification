fb2fc4b4f44ce0c84853fe736eb98379687ee82e
Poorwa Hirve <phirwe@gmail.com>
2019-05-01 15:24:18 -0600

    reorganized repo

--- README.md
+++ README.md
@@ -5,13 +5,8 @@ We are a group of 4 students studying in the University of Colorado, Boulder for
 
 Our project is to build a Siamese Convolutional Neural Network for Authorship Verification.
 
-References **(cloned in the references directory)**:
+References:
 - [OfflineSignatureVerification](https://github.com/Aftaab99/OfflineSignatureVerification.git) by `Aftaab99`
 
 - [Signature-verification-using-deep-learning](https://github.com/jadevaibhav/Signature-verification-using-deep-learning.git) by `jadevaibhav`
-
-> NOTE: References to be added here
-> - When you want to add another git repo in yours (cloned), add it using the following git command:
->	- `git submodule add <repo-link> dir/repo-name`
-
 
---
+++ baseline/valid.py
@@ -0,0 +1,67 @@
+from torch.utils.data import DataLoader
+from Dataset import *
+from torch import optim
+from torchvision import transforms
+import torchvision
+import numpy as np
+from Model_Baseline import *
+import matplotlib.pyplot as plt
+import os
+import sys
+import argparse
+
+# Initialize model, loss and optimizer
+model = BaselineSiamese()
+
+# Parse command line flags
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", "--cuda", action="store_true")
+parser.add_argument("-e", "--epochs", type=int, default=50)
+parser.add_argument("--load_checkpoint", type=str, default=None)
+args = parser.parse_args()
+
+if args.load_checkpoint:
+    checkpoint_path = args.load_checkpoint
+    checkpoint = torch.load(checkpoint_path)
+    model.load_state_dict(checkpoint['state_dict'])
+    model.eval()
+
+# Initialize cuda
+if args.cuda:
+    torch.cuda.set_device(0)
+    device = torch.device("cuda:0")
+    model.cuda()
+
+# Constants from Authors100 dataset
+MAXWIDTH = 2260
+MAXHEIGHT = 337
+
+valid_dataset = AuthorsDataset(
+    root_dir='Dataset',
+    path='train.txt',
+    transform=transforms.Compose([
+        Pad((MAXWIDTH, MAXHEIGHT)),
+        Threshold(177),
+        Downsample(0.75),
+        CropWidth(1000)
+    ]))
+
+valid_loader = DataLoader(
+    valid_dataset,
+    batch_size=1,
+    shuffle=True
+)
+
+acc = 0
+
+for batch_idx,(X1,X2,Y) in enumerate(valid_loader):
+
+    if args.cuda:
+        X1,X2,Y = X1.to(device),X2.to(device),Y.to(device)
+
+    Y_hat = model.forward(X1,X2)
+    #print(Y_hat.argmax(),Y)
+    if Y == Y_hat.argmax():
+        acc += 1
+
+    print(acc/len(valid_dataset))
